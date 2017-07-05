e6304262ba9455fe6feabd1639098763d3f27a49
Tom Myers <tmyers273@gmail.com>
2017-07-05 17:52:52 -0400

    Adds option to override component name

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
 import Modal from './Modal.vue'
 
 const Plugin = {
-  install (Vue, options = {}) {
-    if (this.installed) {
-      return
+    install (Vue, options = {}) {
+        if (this.installed) {
+            return
+        }
+
+        this.installed = true
+        this.event = new Vue()
+
+        Vue.prototype.$modal = {
+            show (name, params) {
+                Plugin.event.$emit('toggle', name, true, params)
+            },
+
+            hide (name, params) {
+                Plugin.event.$emit('toggle', name, false, params)
+            },
+
+            toggle (name, params) {
+                Plugin.event.$emit('toggle', name, undefined, params)
+            }
+        }
+
+        let component_name = options.component_name ? options.component_name : 'modal';
+
+        Vue.component(component_name, Modal)
     }
-
-    this.installed = true
-    this.event = new Vue()
-
-    Vue.prototype.$modal = {
-      show (name, params) {
-        Plugin.event.$emit('toggle', name, true, params)
-      },
-
-      hide (name, params) {
-        Plugin.event.$emit('toggle', name, false, params)
-      },
-
-      toggle (name, params) {
-        Plugin.event.$emit('toggle', name, undefined, params)
-      }
-    }
-
-    Vue.component('modal', Modal)
-  }
 }
 
 export default Plugin
